a3820e6ef0a5e902c0b89df8587cb71fdd4d996b
Amiriddin <amiriddinsarofiddinov8@gmail.com>
2026-04-24 19:09:09 +0500

    NavLinks&Theme Changer added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,70 @@
 import { BsCart3, BsMoonFill, BsSunFill } from "react-icons/bs";
 import { FaBarsStaggered } from "react-icons/fa6";
 import { NavLink } from "react-router-dom";
+import NavLinks from "./NavLinks";
+import { useEffect, useState } from "react";
+
+const themes = {
+  winter: "winter",
+  dracula: "dracula"
+}
+
+const getThemeFromLocalStorage = () => localStorage.getItem("theme") || themes.winter;
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const changeTheme = () => {
+    let newTheme = theme === themes.winter ? themes.dracula : themes.winter;
+  /*
+const getThemeFromLocalStorage = () =>
+  localStorage.getItem("theme") || "winter";
+  */
+    localStorage.setItem("theme", newTheme);
+    setTheme(newTheme);
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
     <nav className="bg-base-200">
-  <div className="navbar align-element">
-    <div className="navbar-start">
-      <NavLink to="/" className="btn btn-primary text-3xl py-6 flex">
-        C
-      </NavLink>
-      <div className="dropdown">
-        <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <FaBarsStaggered className="w-6 h-6"/>
-        </label>
-        <ul className="menu menu-sm dropdown-content mt-3 bg-base-200 round-box">
-            nav links
-        </ul>
-      </div>
-    </div>
-    <div className="navbar-center">
-      <ul className="menu menu-horizontal">
-        nav links
-      </ul>
-    </div>
-    <div className="navbar-end">
-      <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
-        <div className="indicator">
-            <BsCart3 className="w-6 h-6"/>
-            <span className="indicator-item badge badge-primary badge-sm">0</span>
+      <div className="navbar align-element">
+        <div className="navbar-start">
+          <NavLink to="/" className="btn btn-primary text-3xl py-6 hidden lg:flex">
+            C
+          </NavLink>
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBarsStaggered className="w-6 h-6" />
+            </label>
+            <ul className="menu menu-sm dropdown-content mt-3 bg-base-200 round-box w-45">
+              <NavLinks />
+            </ul>
+          </div>
         </div>
-      </NavLink>
-    </div>
-  </div>
-</nav>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal">
+            <NavLinks />
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <label className="swap swap-rotate" onChange={changeTheme}>
+            <input type="checkbox" className=""/>
+            <BsSunFill className="swap-on w-5 h-5" />
+            <BsMoonFill className="swap-off w-5 h-5" />
+          </label>
+          <NavLink to="/cart" className="btn btn-ghost btn-circle btn-md ml-4">
+            <div className="indicator">
+              <BsCart3 className="w-6 h-6" />
+              <span className="indicator-item badge badge-primary badge-sm">0</span>
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
   )
 }
 
